dfd08578963fc2c2caadb65387c9eb12799f9540
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-04-14 17:25:16 +0200

    wip

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+const assert = (cond, txt) => {
+  if (!cond) {
+    throw new Error(txt)
+  }
+}
+
+/**
+ * Parses a fragment of HTML.
+ *
+ * @param {string} str A text
+ * @returns {Array} A list of node elements
+ */
+export function htmlFragment (str) {
+  const doc = new DOMParser().parseFromString(str, 'text/html')
+  assert(doc, 'Invalid HTML document')
+  const body = doc.querySelector('body')
+  assert(body, 'Invalid HTML document')
+  return Array.from(body.children)
+}
+
+/**
+ * Parses a single html element.
+ *
+ * @param {string} str The HTML fragment
+ * @returns {Element} A single element
+ */
+export function htmlElement (str) {
+  const elems = htmlFragment(str)
+  assert(elems.length === 1, 'HTML fragment doesn\'t contain exactly one element')
+  return elems[0]
+}
